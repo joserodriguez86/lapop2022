0b9eb19e551e9e609b890ae2e62a917273c53c9b
Jose Rodriguez de la Fuente <josedelafuente_86@hotmail.com>
2022-08-23 12:05:31 -0300

    Sumando descriptivos

--- pruebas.R
+++ pruebas.R
@@ -14,7 +14,7 @@ prueba <- lapop2004_2018 %>%
 
 
 TCI <- lapop2004_2018 %>% 
-  filter(wave == 2018) %>% 
+  filter(wave == 2016) %>% 
   group_by(cct1b, country_f) %>% 
   tally() %>% 
   pivot_wider(id_cols = cct1b, names_from = country_f, values_from = n) %>% 
@@ -22,9 +22,37 @@ TCI <- lapop2004_2018 %>%
   adorn_pct_formatting(digits = 1)
 
 Subsidios <- lapop2004_2018 %>% 
-  filter(wave == 2018) %>% 
+  filter(wave == 2016) %>% 
   group_by(wf1, country_f) %>% 
   tally() %>% 
   pivot_wider(id_cols = wf1, names_from = country_f, values_from = n) %>% 
   adorn_percentages(denominator = "col") %>% 
   adorn_pct_formatting(digits = 1)
+
+subsidiosxtci <- lapop2004_2018 %>% 
+  filter(wave == 2016 & country_f %in% "Chile") %>% 
+  group_by(wf1, cct1b) %>% 
+  tally() %>% 
+  pivot_wider(id_cols = wf1, names_from = cct1b, values_from = n) %>%
+  adorn_totals(where = c("row", "col")) %>% 
+  adorn_percentages(denominator = "col") %>% 
+  adorn_pct_formatting(digits = 1)
+
+prueba <- lapop2004_2018 %>% 
+  filter(wave == 2016) %>% 
+  group_by(democracia7, country_f) %>% 
+  tally() %>% 
+  pivot_wider(id_cols = democracia7, names_from = country_f, values_from = n) %>% 
+  adorn_percentages(denominator = "col") %>% 
+  adorn_pct_formatting(digits = 1)
+
+
+prueba <- lapop2004_2018 %>%
+  drop_na(formal) %>% 
+  filter(wave == 2018) %>% 
+  group_by(formal_f, country_f) %>% 
+  tally() %>% 
+  pivot_wider(id_cols = formal_f, names_from = country_f, values_from = n) %>% 
+  adorn_percentages(denominator = "col") %>% 
+  adorn_pct_formatting(digits = 1)
+
